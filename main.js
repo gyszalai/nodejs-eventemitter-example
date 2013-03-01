427f5a92116d7d39fbf38e91d7a2eae9d1b93dff
Gyula Szalai <gyszalai@gmail.com>
2013-03-01 15:40:49 +0100

    Init

---
+++ main.js
@@ -0,0 +1,41 @@
+var net = require('net');
+var events = require("events");
+
+var channel = new events.EventEmitter();
+
+channel.addListener('message', function (msg) {
+   console.log('Message: ' + msg.text);
+});
+
+var clientState = {};
+
+var server = net.createServer(function(socket) {
+    
+    var id = socket.remoteAddress + ":" + socket.remotePort;
+    
+    socket.on('connect', function() {
+        channel.emit('message', {text: "Client connected: " + id});
+        socket.write('Árvíztűrő tükörfúrógép from ' + id + '\n\r', 'utf8');
+    });
+    
+    socket.on('close', function() {
+        channel.emit('message', {text: 'Client disconnected: ' + id});
+        channel.emit('shutdown');
+    });
+    
+    socket.on('data', function(data) {
+        channel.emit('message', {text: data});
+        var msg = data.toString();
+        if (msg.match(/shutdown.*/g)) {
+            socket.destroy();
+        }
+    });
+});
+
+channel.on('shutdown', function () {
+    this.removeAllListeners();
+    console.log('Channel shutting down');
+    server.close();
+});
+
+server.listen(8888);
